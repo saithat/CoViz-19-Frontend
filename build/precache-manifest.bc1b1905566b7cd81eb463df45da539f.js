08125393720fa41e518e718edb595ffe1d0063bc
Rakil Ahmed <imrakilahmed@gmail.com>
2020-07-21 01:24:11 -0400

    ...

---
+++ build/precache-manifest.bc1b1905566b7cd81eb463df45da539f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68df8fe2cceb600041be7ccd7838a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75ec53526aa2e864b35a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ce74d29c67ca88f2d273",
+    "url": "/static/js/2.eead384e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eead384e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ec53526aa2e864b35a",
+    "url": "/static/js/main.d73bf3bd.chunk.js"
+  },
+  {
+    "revision": "bc11404c3e784f5d2ba6",
+    "url": "/static/js/runtime-main.0603c9b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
